c4b804512ff64b38bc1595f486202f4faa6e9b4f
Shuo Chen <shanshuo1992@gmail.com>
2025-01-26 23:23:04 +0800

    Update MVDataSet

--- dataset.py
+++ dataset.py
@@ -27,7 +27,7 @@ class VideoRecord(object):
 class MVDataSet(data.Dataset):
     def __init__(self, root_path, list_file,
                  num_segments=3, new_length=1, modality='RGB',
-                 image_tmpl='_{:03}.jpg', transform=None,
+                 image_tmpl='_{:03}.jpg', view_num=6, total_view=12, transform=None,
                  force_grayscale=False, random_shift=True, test_mode=False):
 
         self.root_path = root_path
@@ -39,6 +39,12 @@ class MVDataSet(data.Dataset):
         self.transform = transform
         self.random_shift = random_shift
         self.test_mode = test_mode
+        self.view_number = view_num
+        self.total_view = total_view
+        print(f'MVDataSet.view_number =  {self.view_number}')
+        print('*****************************************')
+        print(f'total view =  {self.total_view}')
+        print('*****************************************')
 
         if self.modality == 'RGBDiff':
             self.new_length += 1# Diff needs one more image to calculate diff
@@ -46,8 +52,7 @@ class MVDataSet(data.Dataset):
 
     def _load_image(self, directory, idx):
         if self.modality == 'RGB' or self.modality == 'RGBDiff':
-             
-            return [Image.open(directory + self.image_tmpl.format(2*idx-1)).convert('RGB')]
+            return [Image.open(directory + self.image_tmpl.format(idx)).convert('RGB')]
 
         elif self.modality == 'Flow':
             x_img = Image.open(os.path.join(directory, self.image_tmpl.format('x', idx))).convert('L')
@@ -62,48 +67,33 @@ class MVDataSet(data.Dataset):
         :param record: VideoRecord
         :return: list
         """
-        offsets = np.arange(6)
-        # offsets = np.arange(12)
+        offsets = np.arange(self.view_number)
         return offsets + 1
 
     def _get_val_indices(self, record):
-        offsets = np.arange(6)
-        # offsets = np.arange(12)
+        offsets = np.arange(self.view_number)
         return offsets + 1
 
     def _get_test_indices(self, record):
-        offsets = np.arange(6)
-        # offsets = np.arange(12)
+        offsets = np.arange(self.view_number)
         return offsets + 1
 
     def __getitem__(self, index):
         record = self.video_list[index]
 
         if not self.test_mode:
-            segment_indices = self._sample_indices(record) if self.random_shift else self._get_val_indices(record)
+            view_indices = np.linspace(1, self.total_view, self.view_number, dtype=int)
         else:
-            segment_indices = self._get_test_indices(record)
+            view_indices = np.linspace(1, self.total_view, self.view_number, dtype=int)
 
-        return self.get(record, segment_indices,index)
+        return self.get(record, view_indices, index)
 
     def get(self, record, indices, index):
         images = list()
-        offSet = index % 6
-        for seg_ind in indices:
-            #p = seg_ind
-            p = int((seg_ind+offSet) % 6 + 1)
-            seg_imgs = self._load_image(record.path, p)
+        for view_idx in indices:
+            seg_imgs = self._load_image(record.path, view_idx)
             images.extend(seg_imgs)
         process_data1 = self.transform(images)
-        #images = list()
-        #offSet = 6#index%6
-  
-        #for seg_ind in indices:
-        #    p = seg_ind#int((seg_ind+offSet)%6+1)
-        #    seg_imgs = self._load_image(record.path, p,off)
-        #    images.extend(seg_imgs)
-        #process_data2 = self.transform(images)
-
 
         return process_data1,record.label
 
